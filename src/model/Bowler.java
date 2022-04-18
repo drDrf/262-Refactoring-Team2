363b48cfdd91a7b8f5483d703d0aba460e1bc569
Devan Kavalchek <devankav7@gmail.com>
2022-04-18 17:49:52 -0400

    refactored bowler

--- src/model/Bowler.java
+++ src/model/Bowler.java
@@ -18,8 +18,7 @@ package model;/*
  */
 
 /**
- *  Class that holds all bowler info
- *
+ * Class that holds all bowler info
  */
 
 public class Bowler {
@@ -28,42 +27,29 @@ public class Bowler {
     private String nickName;
     private String email;
 
-    public Bowler( String nick, String full, String mail ) {
-	nickName = nick;
-	fullName = full;
-  	email = mail;
+    public Bowler(String nick, String full, String mail) {
+        nickName = nick;
+        fullName = full;
+        email = mail;
     }
-
 
     public String getNickName() {
-
-        return nickName;  
-
+        return nickName;
     }
 
-	public String getFullName ( ) {
-			return fullName;
-	}
-	
-	public String getNick ( ) {
-		return nickName;
-	}
+    public String getFullName() {
+        return fullName;
+    }
 
-	public String getEmail ( ) {
-		return email;	
-	}
-	
-	public boolean equals ( Bowler b) {
-		boolean retval = true;
-		if ( !(nickName.equals(b.getNickName())) ) {
-				retval = false;
-		}
-		if ( !(fullName.equals(b.getFullName())) ) {
-				retval = false;
-		}	
-		if ( !(email.equals(b.getEmail())) ) {
-				retval = false;
-		}
-		return retval;
-	}
+    public String getNick() {
+        return nickName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean equals(Bowler b) {
+        return nickName.equals(b.getNickName()) && fullName.equals(b.getFullName()) && email.equals(b.getEmail());
+    }
 }
